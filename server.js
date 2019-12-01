b2bc0c61d5c6ed364f6990bde263245b13156f25
Ian Andrewson <ianandrewson@gmail.com>
2019-12-01 11:32:14 -0800

    request handler skeleton written

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 let http = require('http');
+const url = require('url');
 
-function start() {
+function start(route) {
     function onRequest(request, response) {
-        console.log('Request received');
+        let pathname = url.parse(request.url).pathname;
+        console.log('Request for' + pathname + ' received');
+        
+        route(pathname);
+        
         response.writeHead(200, { 'Content-Type': 'text/plain' });
         response.write('Hello World');
         response.end();
     }
+    
     http.createServer(onRequest).listen(8888);
     console.log('server has started');
 }
