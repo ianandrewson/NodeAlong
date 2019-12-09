0eedeac57f1d4b1bc2ead9b09316bbfe657b310e
Ian Andrewson <ianandrewson@gmail.com>
2019-12-08 23:02:24 -0800

    stopped just before Handling POST requests, pg 32

--- requestHandlers.js
+++ requestHandlers.js
@@ -3,11 +3,11 @@ const exec = require('child_process').exec;
 function start(response) {
     console.log('request handler start was called.');
 
-    exec('ls -lah', function (error, stdout, stderr) {
+    exec('ls -lah', function(error, stdout, stderr) {
         response.writeHead(200, {'Content-Type': 'text/plain'});
         response.write(stdout);
         response.end();
-    }
+    });
 }
 
 function upload() {
